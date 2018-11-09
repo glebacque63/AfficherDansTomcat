d8fe520decc354ec6da3b99d094d096a6bf2067c
glebacque63 <guillaume.lebacque.tic@gmail.com>
2018-11-09 16:12:55 +0100

    update

---
+++ src/ModuloDivision.java
@@ -0,0 +1,20 @@
+
+public class ModuloDivision {
+
+	public static void main(String[] args)
+	{
+		System.out.println(modulo(7, 5));
+		System.out.println(division(10, 4));
+	}
+	
+	public static int modulo(int a, int b)
+	{
+		return a%b
+	}
+	
+	public static double division(double a, double b)
+	{
+		return (double)a/b;
+	}
+
+}
